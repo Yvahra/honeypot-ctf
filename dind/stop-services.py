34e66ed43a335b85dafb094873568434ed5864ab
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-10 10:52:07 +0200

    Update stop-services.py
    add rm/touch known_hosts

--- dind/stop-services.py
+++ dind/stop-services.py
@@ -5,3 +5,6 @@ for i in range(4):
 
 for i in range(4):
   os.system("docker remove "+str(i+1)+"_c")
+
+#os.system("rm /jail/home/player/.ssh/known_hosts")
+#os.system("touch /jail/home/player/.ssh/known_hosts")
