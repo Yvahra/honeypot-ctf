d804f11e23012370cf6a234fba12f46f1041284b
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-12 19:56:51 +0200

    Update start.sh

--- dind/start.sh
+++ dind/start.sh
@@ -2,7 +2,22 @@
 set -e
 
 # Network
-docker network create --subnet=10.0.0.0/8 --gateway=10.0.0.1 honeynet
+# Check if the 'honeynet' network exists
+if ! docker network inspect honeynet >/dev/null 2>&1; then
+  # Network doesn't exist, create it
+  echo "Network 'honeynet' does not exist. Creating..."
+  docker network create --subnet=10.0.0.0/8 --gateway=10.0.0.1 honeynet
+  if [ $? -eq 0 ]; then
+    echo "Network 'honeynet' created successfully."
+  else
+    echo "Error creating network 'honeynet'."
+    exit 1  # Exit with an error code
+  fi
+else
+  # Network exists
+  echo "Network 'honeynet' already exists."
+fi
+
 
 # Logs
 docker volume create --name sharedLogs
