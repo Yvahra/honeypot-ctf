bc9311bbdda2912527e86fd8e9d347ad8b6cab91
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-24 16:56:33 +0200

    Update start.sh

--- deploy/ssh/start.sh
+++ deploy/ssh/start.sh
@@ -83,7 +83,7 @@ fi
 # BANNER
 
 if [ $SSH_TYPE -ne 7 ]; then
-  echo "Banner /app/ssh/banners/ssh" >> /etc/ssh/sshd_config
+  echo "Banner /app/ssh/banners/ssh" > /var/run/motd.dynamic
 else
   echo "No Real Data"
 fi
