d212164fc440b121f2333a1d5b4703a9ffa2f3b4
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-26 10:41:38 +0200

    Update alarm.py

--- deploy/dind/alarm.py
+++ deploy/dind/alarm.py
@@ -2,6 +2,8 @@
 
 import re
 import os
+import datetime
+import subprocess
 
 # Define patterns that indicate suspicious commands (expand this list!)
 suspicious_patterns = [
@@ -50,6 +52,45 @@ def backup():
         if not os.path.exists("/logs_bck/"+str(container)+"/"):
             os.system("mkdir -p /logs_bck/"+str(container))
         os.system("cp /logs/"+str(container)+"/command_history.log /app/dind/logs/history_ssh"+str(container)+".log")
+
+def save_dind_log():
+    """Logs the last command typed by the user to the specified file, along with a timestamp."""
+    try:
+        # 1. Get the last command from .ash_history
+        #    Use 'tail -n 1' to reliably get only the *last* line, even with multi-line commands.
+        #    'shell=True' is necessary because the shell is expanding the tilde (~).
+        #    'text=True' decodes the bytes to a string.
+
+        last_modified = load("/app/dind/.last_dind_log").split("\n")[0]
+        
+        (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(file)
+
+        if last_modified != "-1" and last_modified != time.ctime(mtime):
+        
+            result = subprocess.run(
+                ["tail", "-n", "1", os.path.expanduser("/jail/home//.ash_history")],
+                capture_output=True,
+                text=True,
+                shell=False  # Use shell=False for security, unless you need shell features.
+            )
+            command = result.stdout.strip()
+    
+            # 2. Get the current timestamp
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+            # 3. Format the log entry
+            log_entry = f"    X  {timestamp} {command}\n"
+    
+            # 4. Append the log entry to the log file
+            try:
+                with open("/logs/-1/.ash_history", "a") as f:
+                    f.write(log_entry)
+                os.system("cp /logs/-1/.ash_history /logs/-1/.command_history.log")
+            except Exception as e:
+                print(f"Error writing to log file {log_file}: {e}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 def send_logs():
     try:  
@@ -108,10 +149,15 @@ def check_logs():
         
 
 def main():
+    f = open("/app/dind/.last_dind_log", "w")
+    f.write("-1\n")
+    f.close()
     # Keep the script running
     for container in range(-1,NB_CONTAINERS):
         init_log(container)
+    os.system("touch /logs/-1/.ash_history")
     while True:
+        save_dind_log()
         check_logs()
 
 if __name__ == "__main__":
