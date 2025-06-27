3f386d3680451e3a2452ee180b89d48c377d9038
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-27 09:48:31 +0200

    Update start.sh

--- deploy/dind/start.sh
+++ deploy/dind/start.sh
@@ -10,11 +10,11 @@ mkdir -p /logs/-1
 mkdir -p /jail//logs
 mount --bind /logs/-1 /jail/logs
 
-session-id=$(bash -c 'uuidgen')
-echo "${session-id}" > /app/dind/.container_id
+sessionID=$(bash -c 'uuidgen')
+echo "${sessionID}" > /app/dind/.container_id
 cat /app/dind/banners/motd >> /etc/motd
 echo "Banner /app/dind/banners/warning" >> /etc/ssh/sshd_config 
-sed -i 's/%session-id%/${session-id}/g' /etc/ssh/sshd_config
+sed -i 's/%session-id%/${sessionID}/g' /etc/ssh/sshd_config
 
 # Start Docker daemon
 dockerd &
